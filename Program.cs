a3b9d0fe7fa09567f87f1764e3b68bb3f8187c35
tcantello <98213766+tcantello@users.noreply.github.com>
2022-02-25 12:19:11 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace week6Assignment
+{
+    class Program
+    {
+        static void ProblemOne()
+        {
+            List<string> friends = new List<string>();
+
+            Console.WriteLine("Enter a name :");
+            string friend = Console.ReadLine();
+            while (friend != " ")
+            {
+                friends.Add(friend);
+            }
+            foreach (var item in friends)
+            {
+                Console.WriteLine("You enterd: " + friend);
+            }
+        }
+
+        static void ProblemTwo()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            ProblemOne();
+            ProblemTwo();
+        }
+    }
+}
