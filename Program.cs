195d386f5fc5f979851596b5e1f785e69574dd87
tcantello <tcantello@my.lonestar.edu>
2022-02-27 14:25:50 -0600

    commit 0227
    02272022 1425

--- Program.cs
+++ Program.cs
@@ -7,23 +7,66 @@ namespace week6Assignment
     {
         static void ProblemOne()
         {
-            List<string> friends = new List<string>();
+            string input = "";
 
-            Console.WriteLine("Enter a name :");
-            string friend = Console.ReadLine();
-            while (friend != " ")
+            List<string> Friends = new List<string>();
+
+            Console.WriteLine("Week 6 Problem 1");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            do
             {
-                friends.Add(friend);
-            }
-            foreach (var item in friends)
+                Console.WriteLine("Enter a name. To finish hit the ENTER button with no name entered.");
+                input = Console.ReadLine();
+
+                Friends.Add(input); 
+                
+            } while (input != "");
+
+            if (Friends.Count - 1 == 1)
             {
-                Console.WriteLine("You enterd: " + friend);
+                Console.WriteLine(Friends[0] + " likes your post");
             }
+            else if (Friends.Count - 1 == 2)
+            {
+                Console.WriteLine(Friends[0] + " and " + Friends[1] + "likes your post");
+            }
+            else
+            {
+                Console.WriteLine(Friends[0] + " , " + Friends[1] + " and " + (Friends.Count - 3) +" others like your post.");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
         }
-
         static void ProblemTwo()
         {
+            Dictionary<char, int> letters = new Dictionary<char, int>();
 
+            Console.WriteLine("Week 6 Problem 2");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Enter a sentence.");
+            string userInput = Console.ReadLine();
+            string userInputLower = userInput.ToLower();
+
+            foreach (char letter in userInputLower)
+            {
+                if (letters.ContainsKey(letter))
+                {
+                    letters[letter] += 1;
+                }
+                else
+                {
+                    letters.Add(letter, 1);
+                }
+            }
+            
+            foreach(KeyValuePair<char, int> kvp in letters)
+            {
+                Console.WriteLine(kvp.Key + " | " + kvp.Value);
+            }
         }
         static void Main(string[] args)
         {
